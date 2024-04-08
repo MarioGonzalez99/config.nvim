7789984a2b22a5e609a5ecf28366cfebe1068803
Luis Mario Gonzalez Diaz <39312530+MarioGonzalez99@users.noreply.github.com>
2024-04-08 11:06:13 -0600

    Update init.lua
    Update default dark theme

--- init.lua
+++ init.lua
@@ -751,8 +751,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa-wave'
-      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-wave'
+      vim.cmd.colorscheme 'kanagawa-dragon'
       -- vim.cmd.colorscheme 'kanagawa-lotus'
 
       -- You can configure highlights by doing something like:
