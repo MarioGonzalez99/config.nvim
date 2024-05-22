fbf5f733fff01c0e0314a7ae8943521f066772b7
mariog <mariogonzalezd99@gmail.com>
2024-05-21 21:11:36 -0600

    Add go plugin

--- init.lua
+++ init.lua
@@ -617,7 +617,20 @@ require('lazy').setup({
       }
     end,
   },
-
+  { -- Go Plugin Support
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
