0c7d0a5908548db7e927b40d425a320837ed84f5
mariog <mariogonzalezd99@gmail.com>
2024-06-17 16:16:12 -0600

    Change neotree options

--- init.lua
+++ init.lua
@@ -923,6 +923,19 @@ require('lazy').setup({
       'MunifTanjim/nui.nvim',
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          follow_current_file = true,
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignore = false,
+          },
+        },
+      }
+    end,
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
